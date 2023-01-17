0d872cc1b1b09524296c833c81d2ad1bdb1da382
mohgh-th <122697477+mohgh-th@users.noreply.github.com>
2023-01-17 15:55:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# master-gh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-36wzk2)
---
+++ index.ts
@@ -0,0 +1,6 @@
+let num, num1, nu: number;
+num = parseInt(prompt('الرجاء ادخال رقم'));
+num1 = parseInt(prompt('الرجاء ادخال رقم'));
+nu = num * num1;
+console.log(nu);
+console.log(num, num1);
